862668470245f2f36a0aa184b5a8e165c8ad4dea
Khanbaba Fatullayev <fatullayev@usf.edu>
2026-01-02 18:31:28 +0400

    Add article parsing functionality and update requirements

--- app/services/ml_model.py
+++ app/services/ml_model.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import torch
 from transformers import AutoTokenizer, DistilBertForSequenceClassification
+from newspaper import Article
 
 class ModelPrediction(Enum):
     FAKE = 0
@@ -10,7 +11,10 @@ tokenizer = AutoTokenizer.from_pretrained("model")
 model = DistilBertForSequenceClassification.from_pretrained("model", use_safetensors=True)
 
 def get_newsletter_text(url: str) -> str:
-    raise NotImplementedError()
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
 
 def predict(text: str) -> ModelPrediction:
     inputs = tokenizer(text[:512], return_tensors="pt")
